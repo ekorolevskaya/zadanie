9cedfad73dd2e5a55c1a59bb628b002f65e60c80
Елена Королевская <alenakorolevskaia@mail.ru>
2016-03-15 17:32:18 +0300

    Cоздание нового контакта в адресной книге

--- add_new.py
+++ add_new.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
+from contact import Contact
 
 def is_alert_present(wd):
     try:
@@ -14,76 +15,103 @@ class add_new(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_add_new(self):
-        success = True
-        wd = self.wd
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, login, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(login)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_contact_page(self, wd):
         wd.find_element_by_link_text("add new").click()
+
+    def create_new_contact(self, wd, contact):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("eLENA")
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Elena")
+        wd.find_element_by_name("firstname").send_keys(contact.name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Yrevna")
+        wd.find_element_by_name("middlename").send_keys(contact.Middle)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Korolevskaya")
+        wd.find_element_by_name("lastname").send_keys(contact.Last_name)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("ekorolevskaya")
+        wd.find_element_by_name("nickname").send_keys(contact.Nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Title")
+        wd.find_element_by_name("title").send_keys(contact.Title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Name")
+        wd.find_element_by_name("company").send_keys(contact.Company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Мира 2")
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Mira 2")
+        wd.find_element_by_name("address").send_keys(contact.Adress)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("656506")
+        wd.find_element_by_name("home").send_keys(contact.Home_telephone)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("89632547821")
+        wd.find_element_by_name("mobile").send_keys(contact.Mobile)
+        # заполняем число и месяц рождения
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[28]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[28]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[12]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[12]").click()
+        # заполняем год рождения
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1992")
+        wd.find_element_by_name("byear").send_keys(contact.year)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("Adres 2")
+        wd.find_element_by_name("address2").send_keys(contact.adres_2)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("dgdrhtj")
+        wd.find_element_by_name("phone2").send_keys(contact.phone2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("segsrhr")
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+        # нажимаем на кнопку Enter
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def return_to_contact_page(self, wd):
         wd.find_element_by_link_text("home page").click()
-        wd.find_element_by_xpath("//table[@id='maintable']//td[.='Korolevskaya']").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
+
+    def test_add_new(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, login="admin", password="secret")
+        self.open_contact_page(wd)
+        self.create_new_contact(wd, Contact(name="Elena", Middle="Yrevna", Last_name="Korolevskaya", Nickname="ekorolevskaya",
+                    Title="Title", Company="Name", Adress="Mira 2", Home_telephone="656506", Mobile="89632547821",
+                    year="1992", adres_2="Adress 2", phone2="dgdrhtj", notes="segsrhr"))
+        self.return_to_contact_page(wd)
+        self.logout(wd)
         ActionChains(wd).double_click(wd.find_element_by_name("user")).perform()
         ActionChains(wd).double_click(wd.find_element_by_name("pass")).perform()
-        self.assertTrue(success)
-    
+
+    def test_add_new_1(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, login="admin", password="secret")
+        self.open_contact_page(wd)
+        self.create_new_contact(wd, Contact(name="test", Middle="", Last_name="", Nickname="",
+                    Title="", Company="", Adress="", Home_telephone="", Mobile="",
+                    year="", adres_2="", phone2="", notes=""))
+        self.return_to_contact_page(wd)
+        self.logout(wd)
+        ActionChains(wd).double_click(wd.find_element_by_name("user")).perform()
+        ActionChains(wd).double_click(wd.find_element_by_name("pass")).perform()
+
+
     def tearDown(self):
         self.wd.quit()
 
---
+++ contact.py
@@ -0,0 +1,17 @@
+class Contact:
+
+    def __init__(self, name, Middle, Last_name, Nickname, Title, Company, Adress, Home_telephone,
+                           Mobile, year, adres_2, phone2, notes):
+        self.name = name
+        self.Middle = Middle
+        self.Last_name = Last_name
+        self.Nickname = Nickname
+        self.Title = Title
+        self.Company = Company
+        self.Adress = Adress
+        self.Home_telephone = Home_telephone
+        self.Mobile = Mobile
+        self.year = year
+        self.adres_2 = adres_2
+        self.phone2 = phone2
+        self.notes = notes
