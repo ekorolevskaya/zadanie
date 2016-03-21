8c1299f014ccec86ed23b9ec64f8fbdf6d39874b
Елена Королевская <alenakorolevskaia@mail.ru>
2016-03-21 11:59:21 +0300

    Задание 2.4

--- test_add_group.py
+++ test_add_group.py
@@ -1,24 +1,92 @@
 # -*- coding: utf-8 -*-
+<<<<<<< HEAD
 # задание 2.4
 import pytest
+=======
+from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
+>>>>>>> parent of 7dbd3b3... Задание 2
 from group import Group
-from application import Application
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
 
+class test_add_group(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
 
+<<<<<<< HEAD
 def test_add_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="new", logo="mimi", comment="comment"))
     app.logout()
+=======
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+>>>>>>> parent of 7dbd3b3... Задание 2
 
-def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="", logo="", comment=""))
-    app.logout()
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
+<<<<<<< HEAD
 
+=======
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd, group):
+        wd.find_element_by_name("new").click()
+        # открытие формы для заполнения полей группы
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.logo)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.comment)
+        # нажимаем на кнопку Сохранить группу
+        wd.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(name="name", logo="logo", comment="comment"))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(name="", logo="", comment=""))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+>>>>>>> parent of 7dbd3b3... Задание 2
