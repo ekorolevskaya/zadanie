a8237c51d0d5b7c8e8f28d6fa627e5a8c769e48a
Елена Королевская <alenakorolevskaia@mail.ru>
2016-03-28 15:49:08 +0300

    Выделен класс для работы с контактами

--- fixture/application_for_contact.py
+++ fixture/application_for_contact.py
@@ -2,24 +2,20 @@
 # задание 4
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
-from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
-class Application:
+class Application_for_contact:
 
     def __init__(self):
-        # контруктор, который инициализирует ссылку на драйвер и на помощников
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
-        self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
-        # навигация
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-
     def destroy(self):
-        # метод разрушает фикстуру
         self.wd.quit()
--- fixture/contact.py
+++ fixture/contact.py
@@ -1,35 +1,19 @@
 # занятие 2
-# задание 4
-from selenium.webdriver.firefox.webdriver import WebDriver
+# задание 5
+
+class ContactHelper:
 
 
-class Application_for_contact:
+    # конструктор
+    def __init__(self, app):
+        self.app = app
 
-    def __init__(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, login, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(login)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_contact_page(self):
-        wd = self.wd
+    def open_page(self):
+        wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
 
-    def create_new_contact(self, contact):
-        wd = self.wd
+    def create(self, contact):
+        wd = self.app.wd
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.name)
@@ -78,13 +62,6 @@ class Application_for_contact:
         # нажимаем на кнопку Enter
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def return_to_contact_page(self):
-        wd = self.wd
+    def return_to_page(self):
+        wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def destroy(self):
-        self.wd.quit()
