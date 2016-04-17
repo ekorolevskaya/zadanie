93a723a3677b464da5646495d59f906cded8146b
Елена Королевская <alenakorolevskaia@mail.ru>
2016-04-17 21:20:57 +0300

    Реализованы проверки сравнения списков для групп

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -2,11 +2,12 @@
 #задание 6
 from model.contact import Contact
 
-
-
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(name="Elena", Middle="Yrevna", Last_name="Korolevskaya", Nickname="ekorolevskaya",
-                    Title="Title", Company="Name", Adress="Mira 2", Home_telephone="656506", Mobile="89632547821",
-                    year="1992", adres_2="Adress 2", phone2="dgdrhtj", notes="segsrhr"))
+        app.contact.create(Contact(name="test"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
