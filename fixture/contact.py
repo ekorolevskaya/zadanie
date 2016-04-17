79c9ef447f4afebcdeac5d2b642ff670341777ba
Елена Королевская <alenakorolevskaia@mail.ru>
2016-04-17 18:06:52 +0300

    Оптимизирован переход между страницами
    Задание 10

--- fixture/contact.py
+++ fixture/contact.py
@@ -10,7 +10,8 @@ class ContactHelper:
 
     def open_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (len(wd.find_elements_by_name("Number of results")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
