90a25b7f2aa38d6bdcdc9817810401a5c951afdd
Елена Королевская <alenakorolevskaia@mail.ru>
2016-04-24 21:02:25 +0300

    Заданиче 12. Тест для модификации группы

--- test/test_modific_contact.py
+++ test/test_modific_contact.py
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
 from model.contact import Contact
 
 
 def test_modific_contact_name(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(name="Elena"))
+        app.contact.create(Contact(name="Elena", Last_name="New"))
     old_contacts = app.contact.get_contact_list()
     contact = Contact(name="Elena modific", Last_name="New")
     contact.id = old_contacts[0].id
     app.contact.modific_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
@@ -19,3 +20,4 @@ def test_modific_contact_name(app):
 #                    Title="Title", Company="Name", Adress="Mira 2", Home_telephone="656506", Mobile="89632547821",
 #                    year="1992", adres_2="Adress 2", phone2="dgdrhtj", notes="segsrhr"))
 #    app.contact.modific_first_contact(Contact(Last_name="Korolevskaya modific"))
+
